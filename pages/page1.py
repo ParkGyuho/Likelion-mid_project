2baff23dbc878d6580a0234a39b5d64286f31b45
ParkGyuho <97093059+ParkGyuho@users.noreply.github.com>
2022-06-10 16:13:21 +0900

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -1,10 +1,62 @@
 import streamlit as st
 import time
 import numpy as np
+import pandas as pd
+import folium
+import plotly.express as px
 
-st.set_page_config(page_title="Plotting Demo", page_icon="📈")
+st.set_page_config(page_title="모범음식점 위치", page_icon="🗺️")
 
-st.markdown("# Plotting Demo")
+st.markdown("# 서울시 모범음식점 위치")
+
+@st.cache
+def load_data():
+    data = pd.read_csv("all_seoul_good_rest_final.csv", encoding='utf-8')
+    return data
+
+df = load_data()
+
+lat = df['위도'].sum()/(len(df)-df[df['위도'] == 0].shape[0])
+long = df['경도'].sum()/(len(df)-df[df['경도'] == 0].shape[0])
+
+m = folium.Map(location=[lat,long], zoom_start=12)
+for bst in df.index[:]:
+    row = df.loc[bst]
+    if row['업태명']=='한식':
+        folium.Circle(
+            radius = 7,
+            location = [row['위도'],row['경도']],
+            tooltip = row['업소명'] + '-' + row['업태명'] + '\t' + row['소재지도로명'],
+            color = "red",
+            fill = True,
+        ).add_to(m)
+    elif row['업태명']=='중국식':
+        folium.Circle(
+            radius = 7,
+            location = [row['위도'],row['경도']],
+            tooltip = row['업소명'] + '-' + row['업태명'] + '\t' + row['소재지도로명'],
+            color = "blue",
+            fill = True,
+        ).add_to(m)
+    elif row['업태명']=='일식':
+        folium.Circle(
+            radius = 7,
+            location = [row['위도'],row['경도']],
+            tooltip = row['업소명'] + '-' + row['업태명'] + '\t' + row['소재지도로명'],
+            color = "green",
+            fill = True,
+        ).add_to(m)
+    else:
+        folium.Circle(
+            radius = 7,
+            location = [row['위도'],row['경도']],
+            tooltip = row['업소명'] + '-' + row['업태명'] + '\t' + row['소재지도로명'],
+            color = "yellow",
+            fill = True,
+        ).add_to(m)
+m
+
+'''
 st.sidebar.header("Plotting Demo")
 st.write(
     """This demo illustrates a combination of plotting and animation with
@@ -31,3 +83,4 @@ progress_bar.empty()
 # this button is not connected to any other logic, it just causes a plain
 # rerun.
 st.button("Re-run")
+'''
